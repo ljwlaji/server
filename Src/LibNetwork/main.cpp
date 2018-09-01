f8e843f2e13e9ca630324cc17ccabd52b4e12d5a
playfun <602809934@qq.com>
2018-09-01 09:29:34 +0800

    commit

--- Src/LibNetwork/main.cpp
+++ Src/LibNetwork/main.cpp
@@ -1,10 +1,14 @@
 #include <SocketServer.h>
 #include <LogRunnable.h>
+#include <Config.h>
 #include <iostream>
 void main()
 {
 	std::shared_ptr<LogRunnable> Log = std::make_shared<LogRunnable>();
 	Log->Start();
+
+	sConfig;
+
 	SocketServer* server = new SocketServer();
 	server->Init(nullptr, 9876);
 	server->Lisiten();
---
+++ Src/Shared/Config.cpp
@@ -0,0 +1,149 @@
+#include "Config.h"
+#include "Log.h"
+#include <iomanip>
+#include <fstream>
+#include <Windows.h>
+
+
+static Config* _Config = nullptr;
+static mutex ConfigInstanceLoc;
+
+Config* Config::GetInstance()
+{
+	if (!_Config)
+	{
+		ThreadLocker loc(ConfigInstanceLoc);
+		if (!_Config)
+			_Config = new Config();
+	}
+	return _Config;
+}
+
+Config::Config()
+{
+	char szModuleFilePath[4096];
+	int n = GetModuleFileNameA(0, szModuleFilePath, 4096);
+	sLog->OutBug(szModuleFilePath);
+#ifdef WIN32
+	szModuleFilePath[strrchr(szModuleFilePath, '\\') - szModuleFilePath + 1] = 0;
+#else
+	szModuleFilePath[strrchr(szModuleFilePath, '/') - szModuleFilePath + 1] = 0;
+#endif
+	WorkUrl = szModuleFilePath;
+}
+
+Config::~Config()
+{
+}
+
+bool Config::SetSource(const char * filename)
+{
+	if (!filename)
+		return false;
+	std::string CurrentUrl = WorkUrl + "/" + filename;
+	ifstream myfile(CurrentUrl.c_str());
+
+	if (!myfile)
+		return false;
+
+	values.clear();
+	string line = "";
+	while (getline(myfile, line))
+	{
+		GetLineInfo(line);
+	}
+	myfile.close();
+	return true;
+}
+
+void Config::GetLineInfo(std::string& pstring)
+{
+	std::string CurrentStr = "";
+	if (!pstring.empty())
+	{
+		CurrentStr = pstring.at(0);
+		if (CurrentStr == "#")
+			return;
+	}
+	std::string Key = "";
+	for (int i = 0; i < pstring.length() + 1000; i++)
+	{
+		CurrentStr = "";
+		if (pstring.empty())
+			return;
+		CurrentStr = pstring.at(0);
+		if (CurrentStr == " ")
+		{
+			pstring.erase(0, 1);
+			continue;
+		}
+		if (CurrentStr == "=")
+		{
+			pstring.erase(0, 1);
+			break;
+		}
+		Key += CurrentStr;
+		pstring.erase(0, 1);
+	}
+	bool StringStart = false;
+	values[Key] = "";
+	CurrentStr = "";
+	for (int i = 0; i < pstring.length() + 1000; i++)
+	{
+		if (pstring.empty())
+		{
+			for (int i = 0; i != 1000; i++)
+			{
+				if (CurrentStr.empty())
+					break;
+				std::string TempS = "";
+				TempS = CurrentStr.at(0);
+				if (TempS == " ")
+				{
+					CurrentStr.erase(0, 1);
+				}
+				else break;
+			}
+			values[Key] = CurrentStr;
+			return;
+		}
+		CurrentStr += pstring.at(0);
+		pstring.erase(0, 1);
+	}
+}
+
+bool Config::GetBoolDefault(const char* KeyName, bool Default)
+{
+	std::string _KeyName = KeyName;
+	if (values.find(_KeyName) == values.end())
+		return Default;
+	else
+		return !!atoi(values[KeyName].c_str());
+}
+
+int Config::GetIntDefault(const char* KeyName, int Default)
+{
+	std::string _KeyName = KeyName;
+	if (values.find(_KeyName) == values.end())
+		return Default;
+	else
+		return atoi(values[KeyName].c_str());
+}
+
+string Config::GetStringDefault(const char* KeyName, std::string Default)
+{
+	std::string _KeyName = KeyName;
+	if (values.find(_KeyName) == values.end())
+		return Default;
+	else
+		return values[KeyName];
+}
+
+float Config::GetFloatDefault(const char* KeyName, float Default)
+{
+	std::string _KeyName = KeyName;
+	if (values.find(_KeyName) == values.end())
+		return Default;
+	else
+		return (float)atof(values[KeyName].c_str());
+}

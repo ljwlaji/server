4e3ae1d1f314624894396a462736cf699ccea0d4
playfun <602809934@qq.com>
2018-09-02 11:21:18 +0800

    HttpRequest

--- Src/UnitTest/TcpClient/client.cpp
+++ Src/UnitTest/TcpClient/client.cpp
@@ -1,5 +1,16 @@
 #include "EasyTcpClient.hpp"
 #include<thread>
+#include <future>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#ifndef WIN32
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#endif
 
 bool g_bRun = true;
 void cmdThread()
@@ -61,9 +72,45 @@ void StartTask()
 		client[n]->Close();
 	}
 }
+#include <iostream>
+#include <fstream>
+#include "HttpRequest.h"
+
+
+int test()
+{
+
+	try
+	{
+		http::Request request("http://www.fire-core.com/sms/update-ip.php");
+
+		// send a get request
+		http::Response response = request.send("GET");
+		std::cout << response.body.data() << std::endl; // print the result
+
+														// send a post request
+		response = request.send("POST", "foo=1&bar=baz", {
+			"Content-Type: application/x-www-form-urlencoded"
+			});
+		std::cout << response.body.data() << std::endl; // print the result
+
+														// pass parameters as a map
+		std::map<std::string, std::string> parameters = { { "foo", "1" },{ "bar", "baz" } };
+		response = request.send("POST", parameters, {
+			"Content-Type: application/x-www-form-urlencoded"
+			});
+		std::cout << response.body.data() << std::endl; // print the result
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Request failed, error: " << e.what() << std::endl;
+	}
+	return 0;
+}
 
 int main()
 {
+	test();
 	std::thread t1(cmdThread);
 	t1.detach();
 	StartTask();

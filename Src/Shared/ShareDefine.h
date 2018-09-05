b05ef5dbbe15178f5688c0e4a06035aafec30c17
playfun <602809934@qq.com>
2018-09-05 11:41:30 +0800

    commit

--- Src/Shared/ShareDefine.h
+++ Src/Shared/ShareDefine.h
@@ -11,7 +11,10 @@ typedef unsigned __int32   uint32;
 typedef unsigned __int16   uint16;
 typedef unsigned __int8    uint8;
 #else
+#include <stdint.h>
+#ifndef uint64_t
 #include <linux/types.h>
+#endif
 typedef int64_t            int64;
 typedef int32_t            int32;
 typedef int16_t            int16;
